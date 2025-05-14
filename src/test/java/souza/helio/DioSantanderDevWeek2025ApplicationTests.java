cd520bc916a331325dc6f0c573255f8db80d68ca
Helio Junior <helio_ss_junior@hotmail.com>
2025-05-14 20:19:10 -0300

    Created project

---
+++ src/test/java/souza/helio/DioSantanderDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package souza.helio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSantanderDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
