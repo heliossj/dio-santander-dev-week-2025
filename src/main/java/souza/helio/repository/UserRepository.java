8d39739e1a7042bfc92c30c665bcd843434ceea2
Helio Junior <helio_ss_junior@hotmail.com>
2025-05-14 22:27:01 -0300

    Implementation domain layer

---
+++ src/main/java/souza/helio/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package souza.helio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import souza.helio.model.User;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+}
